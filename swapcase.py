1f46cb470e47ee73450961200983e029ef291e00
Nathan Garcia <nategarcia.va@gmail.com>
2022-11-29 13:54:34 -0500

    "Swapcase"

--- swapcase.py
+++ swapcase.py
@@ -1,8 +1,16 @@
 # You are given a string and your task is to swap cases.
 # In other words, convert all lowercase letters to uppercase letters and vice versa.
 
+def change(c):
+    if str.islower(c):
+        return str.upper(c)
+    else:
+        return str.lower(c)
+
 def swap_case(s):
-    return
+    return ''.join(map(change, s))
 
 if __name__ == '__main__':
-    
+    s = input()
+    result = swap_case(s)
+    print(result)
